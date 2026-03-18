aef129cc5e861a58da1d8282cb7467efe58d2d56
amzeriouil-droid <amzeriouil@gmail.com>
2026-03-18 23:04:56 +0000

    joining all dataframes

--- dataset/page/car_sharing_dashboard.py
+++ dataset/page/car_sharing_dashboard.py
@@ -5,3 +5,7 @@ def load_data():
     cars = pd.read_csv("data/cars.csv") 
     cities = pd.read_csv("data/cities.csv") 
 return trips, cars, cities 
+# Merge trips with cars (joining on car_id) 
+trips_merged = trips.merge(TO COMPLETE) 
+# Merge with cities for car's city (joining on city_id) 
+trips_merged = trips_merged.merge(TO COMPLETE)
