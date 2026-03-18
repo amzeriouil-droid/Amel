76fc7a19a7ad84c96fa960ccaf01db81d32abceb
amzeriouil-droid <amzeriouil@gmail.com>
2026-03-18 23:59:09 +0000

    read

--- dataset/page/car_sharing_dashboard.py
+++ dataset/page/car_sharing_dashboard.py
@@ -4,3 +4,16 @@ import os
 
 st.set_page_config(page_title="Car Sharing Dashboard", layout="wide")
 st.title("Car Sharing Dashboard")
+BASE_DIR = os.path.dirname(__file__)
+
+@st.cache_data
+def load_data():
+
+    trips = pd.read_csv(os.path.join(BASE_DIR, "../trips.csv"))
+    cars = pd.read_csv(os.path.join(BASE_DIR, "../cars.csv"))
+    cities = pd.read_csv(os.path.join(BASE_DIR, "../cities.csv"))
+
+    return trips, cars, cities
+
+
+trips, cars, cities = load_data()
