df6e03afa324253b7af59779ec3bef92f79dbcda
amzeriouil-droid <amzeriouil@gmail.com>
2026-03-19 00:06:51 +0000

    add

--- dataset/page/car_sharing_dashboard.py
+++ dataset/page/car_sharing_dashboard.py
@@ -31,10 +31,3 @@ trips_merged = trips_merged.merge(
 trips_merged = trips_merged.drop(
     columns=["id_car", "city_id", "id_customer", "id"]
 )
-trips_merged["pickup_time"] = pd.to_datetime(
-    trips_merged["pickup_time"]
-)
-
-trips_merged["pickup_date"] = (
-    trips_merged["pickup_time"].dt.date
-)
